ab9794ed89f7fce50ce9483834cc6d19b4d12c8d
Kamran Zafar <kamranzafar4343@gmail.com>
2023-12-17 20:22:11 +0500

    resolved error

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const container= document.querySelector('container');
-const seat= document.querySelectorAll('.row .seat:not(.occupied)');
+const seats= document.querySelectorAll('.row .seat:not(.occupied)');
+const count= document.getElementById('count');
+const total= document.getElementById('total');
+const movieSelect= document.getElementById('movie');
+
+
+//used +sign to make movieSelect integer
+let ticketPrice= +movieSelect.value;
+
+
+container.addEventListener('click', (e) => {
+console.log(e.target);
+});
